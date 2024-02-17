2aa5269e788d3c21df0423994fe5e40f0d1989cb
Juliocoi <amorim.jc@gmail.com>
2024-02-17 13:51:11 -0300

    Primeiro commit

---
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,17 @@
+﻿namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    public Avaliacao(int nota) 
+    {
+        this.Nota = Math.Max(0, Math.Min(10, nota));
+
+    }
+    public int Nota { get; }
+
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
+}
